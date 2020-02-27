d47a37f2907c428b9fef8ad81f8ad03ed8b70ca3
Kilian-Jutglar <kiliozla@gmail.com>
2020-02-28 00:04:17 +0100

    print de prova borrat

--- main.f90
+++ main.f90
@@ -1,14 +1,13 @@
 program dynamics
 use def_variables
 use read_input
-
+integer ::      i
 call cpu_time(start)
 
 call open_input(un_input)
 call read_parameters(un_input,Npart,dens,Nsteps,temp,dt,Nprint,sigmaLJ,epsLJ,mass,seed)
 call allocate_arrays(Nsteps, Nprint, Npart)
 !call initialize()
-print*,size(pos,1),size(pot)
 !do step =1,Nsteps,1
 !       !! call integrador()
 !       if (mod(step,Nprint) == 0) then
