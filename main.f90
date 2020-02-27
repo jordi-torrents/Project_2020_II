57c72406602eb10002b8d78a1cbea942a536fb10
Kilian-Jutglar <kiliozla@gmail.com>
2020-02-27 23:08:38 +0100

    Initializing some files

---
+++ main.f90
@@ -0,0 +1,18 @@
+program dynamics
+use def_variables
+use read_input
+
+call cpu_time(start)
+
+call open_input(un_input)
+call read_parameters(un_input,Npart,dens,Nsteps,temp,dt,Nprint,sigmaLJ,epsLJ,mass,seed)
+call allocate_arrays(Nsteps, Nprint, Npart)
+!call initialize()
+print*,size(pos,1),size(pot)
+!do step =1,Nsteps,1
+!       !! call integrador()
+!       if (mod(step,Nprint) == 0) then
+!enddo
+call cpu_time(finish)
+print*,'CPU time:',finish-start,'s'
+end program dynamics
