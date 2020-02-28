068af69991d047632a63b8dc7a4a3f9697758c6f
Kilian-Jutglar <kiliozla@gmail.com>
2020-02-28 15:28:13 +0100

    canvis estetics

--- main.f90
+++ main.f90
@@ -1,7 +1,9 @@
 program dynamics
+
+!! Modules used
 use def_variables
 use read_input
-integer ::      i
+
 call cpu_time(start)
 
 call open_input(un_input)
@@ -9,9 +11,12 @@ call read_parameters(un_input,Npart,dens,Nsteps,temp,dt,Nprint,sigmaLJ,epsLJ,mas
 call allocate_arrays(Nsteps, Nprint, Npart)
 !call initialize()
 !do step =1,Nsteps,1
-!       !! call integrador()
+!       call integrador()
 !       if (mod(step,Nprint) == 0) then
+!               call compute_magnitudes()
+!               call print_magnitudes
 !enddo
+!call statistics
 call cpu_time(finish)
 print*,'CPU time:',finish-start,'s'
 end program dynamics
