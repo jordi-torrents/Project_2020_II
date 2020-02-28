d1c3c6ba47ab1f5b44aa2b9c144db325ae6a3ac1
Jordi_Torrents <jordi.torrentsm@gmail.com>
2020-02-28 17:51:06 +0100

    inici de la gdr

--- def_variables.f90
+++ def_variables.f90
@@ -1,14 +1,14 @@
 module def_variables
-  
+
   implicit none
   real(8)                 ::      start, finish
   integer                 ::      fStat
   integer                 ::      un_input=101
-  integer                 ::      Npart, step, Nsteps, Nprint
+  integer                 ::      Npart, step, Nsteps, Nprint, Ngdr
   integer                 ::      seed
   real(8)                 ::      dens, temp, dt, sigmaLJ, epsLJ, mass, L
   real(8), allocatable,dimension(:,:)     ::      pos, vel
-  real(8), allocatable,dimension(:)       ::      E_tot, kin, pot, press , temp_inst
+  real(8), allocatable,dimension(:)       ::      E_tot, kin, pot, press , temp_inst, gdr
 
   contains
 
@@ -22,6 +22,7 @@ module def_variables
     allocate(pot(int(Nsteps/Nprint)))
     allocate(press(int(Nsteps/Nprint)))
     allocate(temp_inst(int(Nsteps/Nprint)))
+    allocate(gdr(Ngdr))
   end subroutine allocate_arrays
 
 end module def_variables
